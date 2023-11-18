1f4aba617ca91b2e06ad5b40f60f259d3bc196a0
이승호 <dltmdgh579@gmail.com>
2023-11-19 01:10:34 +0900

    feat: 인적사항 상세정보 리스트 연결, 통신 (#16)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- frontend/src/components/PersonnelList.js
+++ frontend/src/components/PersonnelList.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PersonnelList = (props) => {
   // const { id, name, dateOfBirth, phone, email, profileImage, address } = props;
 
   return (
     <div>
-      <div>이름 : {props.info.name}</div>
-      <div>주소 : {props.info.address.city}</div>
-      <div>이메일 : {props.info.email}</div>
-      <div>전화번호 : {props.info.phone}</div>
-      <br />
-      <hr />
-      <br />
+      <Link to={"/detail/" + props.info.id}>
+        <div>이름 : {props.info.name}</div>
+        <div>주소 : {props.info.address.city}</div>
+        <div>이메일 : {props.info.email}</div>
+        <div>전화번호 : {props.info.phone}</div>
+        <br />
+        <hr />
+        <br />
+      </Link>
     </div>
   );
 };
--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../page/Home";
-import KindergartenInfo from "../page/personnel-info/KindergartenInfo";
-import HolyKidsInfo from "../page/personnel-info/HolyKidsInfo";
-import PaulCommunityInfo from "../page/personnel-info/PaulCommunityInfo";
-import JoshuaInfo from "../page/personnel-info/JoshuaInfo";
+import KindergartenInfo from "../page/personnel-info/list/KindergartenInfo";
+import HolyKidsInfo from "../page/personnel-info/list/HolyKidsInfo";
+import PaulCommunityInfo from "../page/personnel-info/list/PaulCommunityInfo";
+import JoshuaInfo from "../page/personnel-info/list/JoshuaInfo";
 import Kindergarten from "../page/small-group/Kindergarten";
 import HolyKids from "../page/small-group/HolyKids";
 import PaulCommunity from "../page/small-group/PaulCommunity";
 import Joshua from "../page/small-group/Joshua";
+import PersonnelInfoDetail from "../page/personnel-info/detail/PersonnelInfoDetail";
 
 export default () => (
   <BrowserRouter>
@@ -19,6 +20,9 @@ export default () => (
       <Route path="/holykids" element={<HolyKids />}></Route>
       <Route path="/paul-community" element={<PaulCommunity />}></Route>
       <Route path="/joshua" element={<Joshua />}></Route>
+
+      {/* 인원 상세정보 */}
+      <Route path="/detail/:id" element={<PersonnelInfoDetail />}></Route>
 
       {/* 부서 별 상세 페이지 */}
       <Route path="/kindergarten/info" element={<KindergartenInfo />}></Route>
