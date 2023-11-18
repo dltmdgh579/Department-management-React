ac22788c8575415b77b2adc162675053d17bed24
이승호 <dltmdgh579@gmail.com>
2023-11-19 00:00:00 +0900

    feat: 인적사항 상세보기 페이지 구현 (#16)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

---
+++ frontend/src/components/PersonnelDetail.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const PersonnelList = (props) => {
+  // const { id, name, dateOfBirth, phone, email, profileImage, address } = props;
+
+  return (
+    <div>
+      <div>이름 : {props.info.name}</div>
+      <div>생년월일 : {props.info.address.city}</div>
+      <div>휴대전화 : {props.info.email}</div>
+      <div>집전화 : {props.info.phone}</div>
+      <div>주소 : {props.info.address.city}</div>
+      <div>이메일 : {props.info.email}</div>
+      <div>학교/직장 : {props.info.phone}</div>
+      <div>소속 : {props.info.address.city}</div>
+      <div>가족관계 : {props.info.email}</div>
+      <br />
+      <hr />
+      <br />
+    </div>
+  );
+};
+
+export default PersonnelList;
---
+++ frontend/src/page/personnel-info/detail/PaulCommunityDetail.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PersonnelDetail from "../../../components/PersonnelDetail";
+
+const ListPaulCommunity = () => {
+  // useState
+  const [infoList, setInfoList] = useState([]);
+
+  // useEffect
+  useEffect(() => {
+    const infoData = async () => {
+      // const res = await axios.get("http://localhost:8080/kindergarten/info");
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      return res.data;
+    };
+
+    infoData().then((res) => setInfoList(res));
+  }, []);
+
+  return (
+    <div>
+      <PersonnelDetail key={info.id} info={info} />
+    </div>
+  );
+};
+
+export default ListPaulCommunity;
