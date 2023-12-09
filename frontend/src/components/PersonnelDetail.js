1f39cd4e89f5a5650b16f972ab7fea7980192fec
이승호 <dltmdgh579@gmail.com>
2023-12-10 02:20:25 +0900

    feat: detail 정보 추가
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- frontend/src/components/PersonnelDetail.js
+++ frontend/src/components/PersonnelDetail.js
@@ -6,11 +6,12 @@ const PersonnelList = (props) => {
   return (
     <div>
       <div>이름 : {props.info.name}</div>
-      <div>휴대전화 : {props.info.email}</div>
-      <div>집전화 : {props.info.phone}</div>
+      <div>소속 : {props.info.department}</div>
+      <div>생년월일 : {props.info.dateOfBirth}</div>
+      <div>휴대전화 : {props.info.phone}</div>
       <div>이메일 : {props.info.email}</div>
-      <div>학교/직장 : {props.info.phone}</div>
-      <div>가족관계 : {props.info.email}</div>
+      <div>주소 : {props.info.address}</div>
+      <div>가족관계 : 준비중</div>
       <br />
       <hr />
       <br />
