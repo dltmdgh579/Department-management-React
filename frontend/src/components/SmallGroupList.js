f689b6f76b071f2fdec30586ac052fff15803e70
이승호 <dltmdgh579@gmail.com>
2023-12-14 15:24:25 +0900

    style: department info UI 추가 (#30)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- frontend/src/components/SmallGroupList.js
+++ frontend/src/components/SmallGroupList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import styles from "../css/Department_info.module.css";
 
 const SmallGroupList = (props) => {
   // const { id, name, dateOfBirth, phone, email, profileImage, address } = props;
@@ -7,21 +8,25 @@ const SmallGroupList = (props) => {
   console.log(props);
 
   return (
-    <div>
-      <div>
+    <div className={styles.department_info}>
+      <div className={styles.groups}>
         {props.info.smallGroupInfoList ? (
           <div>
             {/* <Link to={"/joshua/" + props.info.name.toLowerCase().replace(/ /g, "-")}> */}
             {props.info.smallGroupInfoList.map((smallGroup) => (
-              <div smallGroup={smallGroup} key={smallGroup.name}>
-                {smallGroup.name}
+              <div
+                className={styles.group}
+                smallGroup={smallGroup}
+                key={smallGroup.name}
+              >
+                <div className={styles.group_name}>{smallGroup.name}</div>
               </div>
             ))}
           </div>
         ) : null}
         {/* </Link> */}
       </div>
-      <div>
+      <div className={styles.attendance}>
         이번 주 출석 : {props.info.attendance}명 (총 재적{" "}
         {props.info.enrollment}명)
       </div>
