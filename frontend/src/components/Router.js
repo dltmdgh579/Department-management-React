18d4400f17a97b893b364ab2e7dee7705ca653f0
이승호 <dltmdgh579@gmail.com>
2023-12-14 17:26:33 +0900

    refactor: 부서 상세 페이지 조회 통합
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -1,42 +1,24 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../page/Home";
-import KindergartenInfo from "../page/personnel-info/list/KindergartenInfo";
-import HolyKidsInfo from "../page/personnel-info/list/HolyKidsInfo";
-import PaulCommunityInfo from "../page/personnel-info/list/PaulCommunityInfo";
-import JoshuaInfo from "../page/personnel-info/list/JoshuaInfo";
-import Kindergarten from "../page/small-group/Kindergarten";
-import HolyKids from "../page/small-group/HolyKids";
-import PaulCommunity from "../page/small-group/PaulCommunity";
-import Joshua from "../page/small-group/Joshua";
 import AllList from "../page/personnel-info/AllList";
 import PersonnelInfoDetail from "../page/personnel-info/detail/PersonnelInfoDetail";
 import GroupInfo from "../page/small-group/info/GroupInfo";
+import DepartmentInfo from "../page/small-group/DepartmentInfo";
 
 export default () => (
   <BrowserRouter>
     <Routes>
-      {/* 부서 별 인원 리스트 페이지 */}
       <Route path="/" element={<Home />}></Route>
-      <Route path="/kindergarten" element={<Kindergarten />}></Route>
-      <Route path="/holykids" element={<HolyKids />}></Route>
-      <Route path="/paul-community" element={<PaulCommunity />}></Route>
-      <Route path="/joshua" element={<Joshua />}></Route>
+
+      {/* 부서 상세 페이지 */}
+      <Route path="/:department" element={<DepartmentInfo />}></Route>
 
       {/* 전체 인원 리스트 페이지 */}
       <Route path="/list" element={<AllList />}></Route>
 
       {/* 인원 상세정보 */}
       <Route path="/detail/:id" element={<PersonnelInfoDetail />}></Route>
-
-      {/* 부서 별 상세 페이지 */}
-      <Route path="/kindergarten/info" element={<KindergartenInfo />}></Route>
-      <Route path="/holykids/info" element={<HolyKidsInfo />}></Route>
-      <Route
-        path="/paul-community/info"
-        element={<PaulCommunityInfo />}
-      ></Route>
-      <Route path="/joshua/info" element={<JoshuaInfo />}></Route>
 
       {/* 부서 별 그룹 상세페이지 */}
       <Route path="/:department/:group" element={<GroupInfo />}></Route>
