d172ea62724cf86524d6b458b3ec518e9156c69a
이승호 <dltmdgh579@gmail.com>
2023-12-18 18:15:19 +0900

    style: list header ui 수정
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- frontend/src/components/personnel/list/PersonnelListHeader.js
+++ frontend/src/components/personnel/list/PersonnelListHeader.js
@@ -1,30 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "../../../css/personnel/list/Personnel_list.module.css";
+import styles from "../../../css/personnel/list/Personnel_list_header.module.css";
 
 const PersonnelListHeader = (props) => {
   return (
     <div className={styles.container}>
-      <div>
-        <Link to="/personnel/post">추가</Link>
+      <div className={styles.post}>
+        <Link to="/personnel/post" className={styles.post_link}>
+          추가
+        </Link>
       </div>
-      <div>필터</div>
-      <div>검색</div>
-      {/* <Link to="/personnel" className={styles.link}>
-        <div className={styles.info_container}>
-          <img
-            src="https://d1qll2sj38w7uy.cloudfront.net/member/default/1.jpg"
-            className={styles.profile_image}
-          ></img>
-          <div className={styles.text_container}>
-            <div className={styles.text_name}>{name}</div>
-            <div>{dateOfBirth}</div>
-            <div>{phone}</div>
-            <div>{address}</div>
-          </div>
-        </div>
-        <hr />
-      </Link> */}
+      <div className={styles.filter}>필터</div>
+      <div className={styles.search_container}>
+        <input className={styles.search}></input>
+      </div>
     </div>
   );
 };
