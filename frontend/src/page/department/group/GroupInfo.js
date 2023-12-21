c4e2cbbae4c48644301b4f223c2d0d6c6f3b2903
이승호 <dltmdgh579@gmail.com>
2023-12-21 22:53:33 +0900

    feat: 결석 체크된 인원 API Request (#43)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- frontend/src/page/department/group/GroupInfo.js
+++ frontend/src/page/department/group/GroupInfo.js
@@ -4,12 +4,15 @@ import GroupInfoList from "../../../components/department/group/GroupInfoList";
 import GroupInfoHeader from "./header/GroupInfoHeader";
 import { useParams } from "react-router-dom";
 import styles from "../../../css/department/group/Group_info_list.module.css";
+import moment from "moment";
 
 const GroupInfo = () => {
   const department = useParams().department;
   const group = useParams().group;
 
   const [infoList, setInfoList] = useState([]);
+  const [isCheck, setCheck] = useState(false);
+  const [absenteeList, setAbsenteeList] = useState([]);
 
   useEffect(() => {
     const infoData = async () => {
@@ -23,14 +26,69 @@ const GroupInfo = () => {
     infoData().then((res) => setInfoList(res));
   }, []);
 
+  const handleAbsent = (e) => {
+    if (e.target.checked === false) {
+      setCheck(false);
+    } else if (e.target.checked === true) {
+      setCheck(true);
+    }
+  };
+
+  const checkAbsentee = (absenteeInfo) => {
+    const id = absenteeInfo.id;
+    const name = absenteeInfo.name;
+    const absentDate = moment().format("YYYY-MM-DD");
+    const isAbsent = absenteeInfo.isAbsent;
+    if (isAbsent === true) {
+      setAbsenteeList([...absenteeList, { id, name, absentDate }]);
+    } else if (isAbsent === false) {
+      setAbsenteeList(absenteeList.filter((absentee) => absentee.id != id));
+    }
+  };
+
+  const sendAbsenteeList = async () => {
+    await axios({
+      method: "post",
+      url: "http://localhost:8080/" + department + "/" + group + "/absent",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      data: JSON.stringify({ absenteeList: absenteeList }),
+    }).then((response) => {
+      setCheck(false);
+    });
+  };
+
   return (
-    <div>
+    <div className={styles.parent_container}>
       <GroupInfoHeader params={[department, group]} />
-      <div className={styles.parent_container}>
+      <div className={styles.toggle_switch_container}>
+        <label className={styles.toggle_switch}>
+          <input
+            type="checkbox"
+            checked={isCheck}
+            onChange={(e) => handleAbsent(e)}
+          ></input>
+          <span className={styles.slider_round}></span>
+        </label>
+        <div>출결관리</div>
+      </div>
+      <div className={styles.group_info_container}>
         {infoList.map((info) => (
-          <GroupInfoList key={info.id} info={info} />
+          <GroupInfoList
+            key={info.id}
+            info={info}
+            check={isCheck}
+            checkFunction={checkAbsentee}
+          />
         ))}
       </div>
+      {isCheck ? (
+        <div className={styles.done_check} onClick={sendAbsenteeList}>
+          ✔
+        </div>
+      ) : null}
     </div>
   );
 };
