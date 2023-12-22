87b5e1cb6d2f42b0a8b317e13d53d48b0cf5efa0
이승호 <dltmdgh579@gmail.com>
2023-12-22 17:57:20 +0900

    feat: 그룹에서 인원 추가하는 페이지 작성 (#45)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

---
+++ frontend/src/page/department/group/AddGroupMember.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PersonnelList from "../../../components/personnel/list/PersonnelList";
+import PersonnelListHeader from "../../../components/personnel/list/PersonnelListHeader";
+import { useLocation, useNavigate } from "react-router-dom";
+import styles from "../../../css/department/group/Group_add_member.module.css";
+import moment from "moment";
+
+const AddGroupMember = (props) => {
+  const location = useLocation();
+  const departmentId = location.state?.departmentId;
+  const groupId = location.state?.groupId;
+
+  const [infoList, setInfoList] = useState([]);
+  const [addMemberList, setAddMemberList] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const infoData = async () => {
+      const res = await axios.get("http://dnch-edu.com:8080/list");
+      return res.data;
+    };
+
+    infoData().then((res) => setInfoList(res));
+  }, []);
+
+  const checkAddMemberInfo = (addMemberInfo) => {
+    const id = addMemberInfo.id;
+    const name = addMemberInfo.name;
+    const addDate = moment().format("YYYY-MM-DD");
+    const isAddMember = addMemberInfo.isAddMember;
+    if (isAddMember === true) {
+      setAddMemberList([...addMemberList, { id, name, addDate }]);
+    } else if (isAddMember === false) {
+      setAddMemberList(addMemberList.filter((member) => member.id != id));
+    }
+  };
+
+  const sendAddMemberList = async () => {
+    await axios({
+      method: "post",
+      url: "http://dnch-edu.com:8080/" + departmentId + "/" + groupId + "/add",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      data: JSON.stringify({ addMemberList: addMemberList }),
+    }).then((response) => {
+      navigate(`/${departmentId}/${groupId}`);
+    });
+  };
+
+  return (
+    <div className={styles.parent_container}>
+      <PersonnelListHeader add={true} />
+      {infoList.map((info) => (
+        <PersonnelList
+          key={info.id}
+          info={info}
+          add={true}
+          checkFunction={checkAddMemberInfo}
+        />
+      ))}
+      <div className={styles.done_check} onClick={sendAddMemberList}>
+        ✔
+      </div>
+    </div>
+  );
+};
+
+export default AddGroupMember;
--- frontend/src/page/department/group/header/GroupInfoHeader.js
+++ frontend/src/page/department/group/header/GroupInfoHeader.js
@@ -3,8 +3,19 @@ import { Link } from "react-router-dom";
 import styles from "../../../../css/department/group/Group_info_header.module.css";
 
 const GroupInfoHeader = (props) => {
+  const departmentId = props.params[0];
+  const groupId = props.params[1];
+
   return (
     <div className={styles.container}>
+      <div className={styles.add_personnel}>
+        <Link
+          to="/list/add"
+          state={{ departmentId: departmentId, groupId: groupId }}
+        >
+          추가
+        </Link>
+      </div>
       <div className={styles.long_term_absentee}>장기 결석자</div>
       <div className={styles.absentee}>
         <Link
