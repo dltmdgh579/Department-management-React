68edae25d412ef88f3a2b16df4059e833bc60dc3
이승호 <dltmdgh579@gmail.com>
2023-12-23 23:55:21 +0900

    feat: 그룹 추가 기능 작성 (#46)
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

---
+++ frontend/src/components/department/AddGroup.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import styles from "../../css/department/Add_group.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AddGroup = (props) => {
+  const departmentId = props.state.id;
+  const departmentName = props.state.name;
+
+  const [newGroup, setNewGroup] = useState([]);
+
+  const navigate = useNavigate();
+
+  const addNewGroup = async () => {
+    await axios({
+      method: "post",
+      url: "http://dnch-edu.com:8080/" + departmentId,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      data: JSON.stringify({ name: newGroup }),
+    }).then(() => {
+      navigate("/" + departmentName, { state: props.state });
+      window.location.reload();
+    });
+  };
+
+  const inputGroupName = (e) => {
+    setNewGroup(e.target.value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <input className={styles.input_group} onChange={inputGroupName}></input>
+      <div className={styles.add_button} onClick={addNewGroup}>
+        +
+      </div>
+    </div>
+  );
+};
+
+export default AddGroup;
--- frontend/src/page/department/DepartmentInfo.js
+++ frontend/src/page/department/DepartmentInfo.js
@@ -1,24 +1,45 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import SmallGroupList from "../../components/department/GroupList";
+import GroupList from "../../components/department/GroupList";
 import { useLocation } from "react-router-dom";
+import DepartmentInfoHeader from "./DepartmentInfoHeader";
+import GroupAttendance from "../../components/department/GroupAttendance";
+import AddGroup from "../../components/department/AddGroup";
 
 const DepartmentInfo = (props) => {
   const { state } = useLocation();
+  const departmentId = state?.id;
+  const departmentName = state?.name;
   const [info, setInfo] = useState([]);
+  const [isAddGroup, setAddGroup] = useState(false);
+  const [isModifyGroup, setModifyGroup] = useState(false);
 
   useEffect(() => {
     const infoData = async () => {
-      const res = await axios.get("http://dnch-edu.com:8080/" + state.id);
+      const res = await axios.get("http://dnch-edu.com:8080/" + departmentId);
       return res.data;
     };
 
     infoData().then((res) => setInfo(res));
   }, []);
 
+  const addGroup = (isAdd) => {
+    setAddGroup(isAdd);
+  };
+
+  const modifyGroup = (isModify) => {
+    setModifyGroup(isModify);
+  };
+
   return (
     <div>
-      <SmallGroupList key={info.id} info={info} state={state} />
+      <DepartmentInfoHeader
+        addFunction={addGroup}
+        modifyFunction={modifyGroup}
+      />
+      {isAddGroup ? <AddGroup state={state} /> : null}
+      <GroupList key={info.id} info={info} state={state} />
+      <GroupAttendance info={[info.attendance, info.enrollment]} />
     </div>
   );
 };
