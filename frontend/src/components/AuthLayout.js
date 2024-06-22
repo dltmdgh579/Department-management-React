768b4089d39525f5b37033a0ff8c7d516cd144b1
이승호 <dltmdgh579@gmail.com>
2024-06-23 00:02:21 +0900

    auth: remain login status by remember-me cookie
    Signed-off-by: 이승호 <dltmdgh579@gmail.com>

--- frontend/src/components/AuthLayout.js
+++ frontend/src/components/AuthLayout.js
@@ -1,12 +1,29 @@
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_ROOT = process.env.REACT_APP_API_ROOT;
 
 const AuthLayout = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     if (sessionStorage.getItem("Authorized") !== "OK") {
-      navigate("/login");
+      axios({
+        method: "get",
+        url: `${API_ROOT}`,
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            sessionStorage.setItem("Authorized", "OK");
+            navigate("/");
+          } else {
+            navigate("/login");
+          }
+        })
+        .catch(() => {
+          navigate("/login");
+        });
     }
   }, []);
 
